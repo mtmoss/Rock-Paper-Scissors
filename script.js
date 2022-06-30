784c2795284541034b4251dc5715fcffdc448c84
mtmoss <mt@mtmoss.com.br>
2022-06-30 17:01:35 -0300

    User input is now non-case sensitive

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ function computerPlay() {
     return plays[Math.floor(Math.random()*plays.length)];
 }
 
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection == computerSelection) {
-        return `We both chose ${playerSelection}. It\'s a draw!`;
-    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        return `You chose ${playerSelection} and I chose ${computerSelection}. You win...`;
-    } else if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        return `You chose ${playerSelection} and I chose ${computerSelection}. Ha! You lose!`;
+function playRound(a,b) {
+    const aLowerCase = a.toLowerCase();
+    if (aLowerCase == b) {
+        return `We both chose ${aLowerCase}. It\'s a draw!`;
+    } else if ((aLowerCase == 'rock' && b == 'scissors') || (aLowerCase == 'paper' && b == 'rock') || (aLowerCase == 'scissors' && b == 'paper')) {
+        return `You chose ${aLowerCase} and I chose ${b}. Fine, you win...`;
+    } else if ((aLowerCase == 'rock' && b == 'paper') || (aLowerCase == 'paper' && b == 'scissors') || (aLowerCase == 'scissors' && b == 'rock')) {
+        return `You chose ${aLowerCase} and I chose ${b}. Ha! You lose!`;
     };
 }
 
-let playerSelection = prompt('What will you choose? Write "rock", "paper" or "scissors".');
+let playerSelection = prompt('Can you beat me? Enter "rock", "paper" or "scissors".');
 let computerSelection = computerPlay();
 console.log(playRound(playerSelection,computerSelection));
